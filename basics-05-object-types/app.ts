695b9d4303a187f6cf04971a0992611c787ba057
Hai-Au Bui <haiaunht@yahoo.com>
2021-10-18 17:00:41 -0700

    TS_config, Compiler, and VS Code TS debugging

--- basics-05-object-types/app.ts
+++ basics-05-object-types/app.ts
@@ -1,4 +1,4 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number): number {
   return n1 + n2;
 }
 
@@ -9,10 +9,10 @@ combineValue = add;
 console.log(combineValue(8,8));
 
 
-//call back function
-function printResult (num : number): void {
-  console.log("Result: " + num);
-}
+// //call back function
+// function printResult (num : number): void {
+//   console.log("Result: " + num);
+// }
 
 function addAndPrint(n1: number, n2: number, callback: (result: number) => void) {
   const result = n1 + n2;
